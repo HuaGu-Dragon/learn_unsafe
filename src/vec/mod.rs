b1e0ba25987bb9e804b2a5b85cfbc2e9293c33bb
俺不叫化咕龙 <1801943622@qq.com>
2025-06-28 16:02:27 +0800

    Implement IntoIterator and Iterator traits for custom Vec<T> type, including double-ended iteration support

--- src/vec/mod.rs
+++ src/vec/mod.rs
@@ -1,5 +1,6 @@
 use std::{
     alloc::Layout,
+    mem::ManuallyDrop,
     ops::{Deref, DerefMut},
     ptr::NonNull,
 };
@@ -9,6 +10,13 @@ pub struct Vec<T> {
     ptr: NonNull<T>,
     len: usize,
     cap: usize,
+}
+
+pub struct IntoIter<T> {
+    buf: NonNull<T>,
+    cap: usize,
+    start: *const T,
+    end: *const T,
 }
 
 unsafe impl<T: Send> Send for Vec<T> {}
@@ -125,9 +133,60 @@ impl<T> Vec<T> {
     }
 }
 
+impl<T> IntoIterator for Vec<T> {
+    type Item = T;
+    type IntoIter = IntoIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        let vec = ManuallyDrop::new(self);
+        let ptr = vec.ptr;
+        let len = vec.len;
+        let cap = vec.cap;
+        IntoIter {
+            buf: ptr,
+            cap,
+            start: ptr.as_ptr(),
+            end: if cap == 0 {
+                // can not use `ptr.as_ptr().add(len)` here because it will panic if len is 0
+                ptr.as_ptr()
+            } else {
+                unsafe { ptr.as_ptr().add(len) }
+            },
+        }
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.start == self.end {
+            return None;
+        }
+        let value = unsafe { std::ptr::read(self.start) };
+        self.start = unsafe { self.start.add(1) };
+        Some(value)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let remaining = unsafe { self.end.offset_from(self.start) } as usize;
+        (remaining, Some(remaining))
+    }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.start == self.end {
+            return None;
+        }
+        self.end = unsafe { self.end.sub(1) };
+        let value = unsafe { std::ptr::read(self.end) };
+        Some(value)
+    }
+}
+
 impl<T> Deref for Vec<T> {
     type Target = [T];
-
     fn deref(&self) -> &Self::Target {
         unsafe { std::slice::from_raw_parts(self.ptr.as_ptr(), self.len) }
     }
@@ -150,6 +209,24 @@ impl<T> Drop for Vec<T> {
                 // Deallocate the memory
                 std::alloc::dealloc(
                     self.ptr.as_ptr() as *mut u8,
+                    Layout::array::<T>(self.cap).unwrap(),
+                );
+            }
+        }
+    }
+}
+
+impl<T> Drop for IntoIter<T> {
+    fn drop(&mut self) {
+        if self.cap != 0 {
+            for i in 0..self.cap {
+                unsafe {
+                    std::ptr::drop_in_place(self.buf.as_ptr().add(i));
+                }
+            }
+            unsafe {
+                std::alloc::dealloc(
+                    self.buf.as_ptr() as *mut u8,
                     Layout::array::<T>(self.cap).unwrap(),
                 );
             }
@@ -279,5 +356,43 @@ mod tests {
         assert_eq!(vec[0], 10);
         assert_eq!(vec[1], 20);
         assert_eq!(vec[2], 30);
+    }
+
+    #[test]
+    fn test_into_iter() {
+        let mut vec = Vec::new();
+        vec.push(1);
+        vec.push(2);
+        vec.push(3);
+
+        let mut iter = vec.into_iter();
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), None); // Should return None when empty
+
+        // Test size_hint
+        let (lower, upper) = iter.size_hint();
+        assert_eq!(lower, 0);
+        assert_eq!(upper, Some(0));
+    }
+
+    #[test]
+    fn test_into_iter_double_ended() {
+        let mut vec = Vec::new();
+        vec.push(1);
+        vec.push(2);
+        vec.push(3);
+
+        let mut iter = vec.into_iter();
+        assert_eq!(iter.next_back(), Some(3));
+        assert_eq!(iter.next_back(), Some(2));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next_back(), None); // Should return None when empty
+
+        // Test size_hint
+        let (lower, upper) = iter.size_hint();
+        assert_eq!(lower, 0);
+        assert_eq!(upper, Some(0));
     }
 }
