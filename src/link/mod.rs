a510eaea41d70f9c2d6ae6114eca9b93498559a4
俺不叫化咕龙 <1801943622@qq.com>
2025-07-04 19:59:58 +0800

    Enhance List<T> implementation with front/back accessors and iterator support

--- src/link/mod.rs
+++ src/link/mod.rs
@@ -25,6 +25,18 @@ impl<T> List<T> {
         }
     }
 
+    pub fn is_empty(&self) -> bool {
+        self.len == 0
+    }
+
+    pub fn front(&self) -> Option<&T> {
+        self.head.map(|node| unsafe { &node.as_ref().elem })
+    }
+
+    pub fn front_mut(&mut self) -> Option<&mut T> {
+        self.head.map(|mut node| unsafe { &mut node.as_mut().elem })
+    }
+
     pub fn push_front(&mut self, elem: T) {
         let new_node = unsafe {
             NonNull::new_unchecked(Box::into_raw(Box::new(Node {
@@ -62,6 +74,14 @@ impl<T> List<T> {
             }
             elem
         })
+    }
+
+    pub fn back(&self) -> Option<&T> {
+        self.tail.map(|node| unsafe { &node.as_ref().elem })
+    }
+
+    pub fn back_mut(&mut self) -> Option<&mut T> {
+        self.tail.map(|mut node| unsafe { &mut node.as_mut().elem })
     }
 
     pub fn push_back(&mut self, elem: T) {
@@ -110,6 +130,60 @@ impl<T> List<T> {
     }
 }
 
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        while let Some(_) = self.pop_front() {
+            // Continuously pop elements until the list is empty
+        }
+    }
+}
+
+pub struct Iter<'a, T> {
+    front: Link<T>,
+    back: Link<T>,
+    len: usize,
+    _marker: std::marker::PhantomData<&'a T>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.len != 0 {
+            self.front.map(|node| {
+                self.len -= 1;
+                self.front = unsafe { (*node.as_ptr()).back };
+                unsafe { &node.as_ref().elem }
+            })
+        } else {
+            None
+        }
+    }
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
+}
+
+impl<'a, T> DoubleEndedIterator for Iter<'a, T> {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.len != 0 {
+            self.back.map(|node| {
+                self.len -= 1;
+                self.back = unsafe { (*node.as_ptr()).front };
+                unsafe { &node.as_ref().elem }
+            })
+        } else {
+            None
+        }
+    }
+}
+
+impl<'a, T> ExactSizeIterator for Iter<'a, T> {
+    fn len(&self) -> usize {
+        self.len
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -124,5 +198,66 @@ mod tests {
 
         list.push_front(2);
         assert_eq!(list.len(), 2);
+    }
+
+    #[test]
+    fn test_push_pop() {
+        let mut list = List::new();
+        list.push_back(1);
+        list.push_back(2);
+        assert_eq!(list.len(), 2);
+        assert_eq!(list.pop_front(), Some(1));
+        assert_eq!(list.pop_back(), Some(2));
+        assert_eq!(list.len(), 0);
+    }
+
+    #[test]
+    fn test_front_back() {
+        let mut list = List::new();
+        list.push_front(1);
+        list.push_back(2);
+        assert_eq!(list.front(), Some(&1));
+        assert_eq!(list.back(), Some(&2));
+        assert_eq!(list.len(), 2);
+
+        if let Some(front) = list.front_mut() {
+            *front = 3;
+        }
+        if let Some(back) = list.back_mut() {
+            *back = 4;
+        }
+        assert_eq!(list.front(), Some(&3));
+        assert_eq!(list.back(), Some(&4));
+    }
+
+    #[test]
+    fn test_empty_list() {
+        let mut list: List<i32> = List::new();
+        assert!(list.is_empty());
+        assert_eq!(list.front(), None);
+        assert_eq!(list.back(), None);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.pop_back(), None);
+    }
+
+    #[test]
+    fn test_drop() {
+        struct DropItem(i32);
+
+        impl Drop for DropItem {
+            fn drop(&mut self) {
+                println!("Dropping {}", self.0);
+            }
+        }
+
+        {
+            let mut list = List::new();
+            list.push_front(DropItem(1));
+            list.push_back(DropItem(2));
+            assert_eq!(list.len(), 2);
+        } // List goes out of scope and should drop all elements
+
+        let list: List<i32> = List::new();
+        assert!(list.is_empty());
     }
 }
