a73e7c5c944879f34cc8fa0238b129743dfa334b
俺不叫化咕龙 <1801943622@qq.com>
2025-07-12 14:39:32 +0800

    Add tests for SpinLock to verify single-threaded and multi-threaded behavior

--- src/spinlock/mod.rs
+++ src/spinlock/mod.rs
@@ -27,3 +27,52 @@ impl SpinLock {
         self.locked.store(false, Ordering::Release);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn spinlock_single_threaded() {
+        let lock = SpinLock::new();
+        let mut data = vec![];
+
+        lock.lock();
+        data.push(1);
+        lock.unlock();
+
+        lock.lock();
+        print!("{:?}", data);
+        lock.unlock();
+    }
+
+    #[test]
+    fn spinlock_multi_threaded() {
+        use std::thread;
+
+        let lock = SpinLock::new();
+        let mut data = vec![];
+
+        thread::scope(|s| {
+            s.spawn(|| {
+                lock.lock();
+                let data_ptr = &raw const data as *mut Vec<i32>;
+                unsafe {
+                    (*data_ptr).push(1);
+                }
+                lock.unlock();
+            });
+            thread::sleep(std::time::Duration::from_millis(100));
+            lock.lock();
+            let data_ptr = &raw const data as *mut Vec<i32>;
+            unsafe {
+                (*data_ptr).push(2);
+            }
+            lock.unlock();
+        });
+        lock.lock();
+        data.push(3);
+        println!("{:?}", data);
+        lock.unlock();
+    }
+}
