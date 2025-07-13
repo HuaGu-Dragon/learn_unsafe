313f8a34fec5974c7837efe990dd041fcd9060f6
俺不叫化咕龙 <1801943622@qq.com>
2025-07-13 15:47:23 +0800

    Refactor Channel implementation to improve safety and usability in send/recv methods

--- src/one_shot/mod.rs
+++ src/one_shot/mod.rs
@@ -1,13 +1,57 @@
 use std::{
     cell::UnsafeCell,
     mem::MaybeUninit,
-    sync::atomic::{AtomicBool, Ordering},
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
 };
 
 pub struct Channel<T> {
     message: UnsafeCell<MaybeUninit<T>>,
     ready: AtomicBool,
-    used: AtomicBool,
+}
+
+pub struct Sender<T> {
+    channel: Arc<Channel<T>>,
+}
+
+impl<T> Sender<T> {
+    pub fn send(self, message: T) {
+        unsafe {
+            (*self.channel.message.get()).write(message);
+        }
+        self.channel.ready.store(true, Ordering::Release);
+    }
+}
+
+pub struct Receiver<T> {
+    channel: Arc<Channel<T>>,
+}
+
+impl<T> Receiver<T> {
+    pub fn is_ready(&self) -> bool {
+        self.channel.ready.load(Ordering::Relaxed)
+    }
+    pub fn recv(self) -> T {
+        if !self.channel.ready.swap(false, Ordering::Release) {
+            panic!("Only single message allowed");
+        };
+        unsafe {
+            // SAFETY: We assume the message is ready to be read
+            (*self.channel.message.get()).assume_init_read()
+        }
+    }
+}
+
+pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
+    let channel = Arc::new(Channel::new());
+    (
+        Sender {
+            channel: channel.clone(),
+        },
+        Receiver { channel },
+    )
 }
 
 impl<T> Channel<T> {
@@ -15,34 +59,7 @@ impl<T> Channel<T> {
         Self {
             message: UnsafeCell::new(MaybeUninit::uninit()),
             ready: AtomicBool::new(false),
-            used: AtomicBool::new(false),
         }
-    }
-
-    /// SAFETY: Caller must ensure that the channel is not ready
-    /// and that no other thread is sending a message at the same time.
-    /// Only call it once.
-    pub unsafe fn send(&self, message: T) {
-        if self.used.swap(true, Ordering::Relaxed) {
-            panic!("Channel is already in use");
-        }
-        unsafe {
-            (*self.message.get()).write(message);
-        }
-        self.ready.store(true, Ordering::Release);
-    }
-
-    pub fn is_ready(&self) -> bool {
-        self.ready.load(Ordering::Acquire)
-    }
-
-    /// Only call this if `is_ready` is true
-    /// SAFETY: Caller must ensure that the channel is ready
-    pub unsafe fn recv(&self) -> T {
-        if !self.ready.swap(false, Ordering::Acquire) {
-            panic!("Channel is not ready");
-        }
-        unsafe { (*self.message.get()).assume_init_read() }
     }
 }
 
@@ -68,39 +85,29 @@ mod tests {
 
     #[test]
     fn test_channel_single_thread() {
-        let channel = Channel::new();
+        let (sender, receiver) = channel();
 
-        // Test sending a message
-        unsafe {
-            channel.send(42);
-        }
+        sender.send(42);
+        assert!(receiver.is_ready());
 
-        // Test receiving a message
-        unsafe {
-            assert!(channel.is_ready());
-            let message = channel.recv();
-            assert_eq!(message, 42);
-        }
+        let res = receiver.recv();
+        assert_eq!(res, 42);
     }
 
     #[test]
     fn test_channel_multi_thread() {
         use std::thread;
 
-        let channel = Channel::new();
+        let (sender, receiver) = channel();
 
         thread::scope(|s| {
-            s.spawn(|| {
+            s.spawn(move || {
                 sleep(Duration::from_millis(100));
-                unsafe {
-                    channel.send(100);
-                }
+                sender.send(42);
             });
-
             loop {
-                if channel.is_ready() {
-                    let res = unsafe { channel.recv() };
-                    assert_eq!(res, 100);
+                if receiver.is_ready() {
+                    assert_eq!(receiver.recv(), 42);
                     break;
                 }
             }
