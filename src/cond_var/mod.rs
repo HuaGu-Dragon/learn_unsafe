c12c69e9e1d6e554f97ed29fbd64ecf538f95a0a
俺不叫化咕龙 <1801943622@qq.com>
2025-07-26 17:27:31 +0800

    Make Condvar struct public and add waiter for tracking active waiters

--- src/cond_var/mod.rs
+++ src/cond_var/mod.rs
@@ -1,37 +1,47 @@
-use std::sync::atomic::{AtomicU32, Ordering};
+use std::sync::atomic::{AtomicU32, AtomicUsize, Ordering};
 
 use atomic_wait::{wait, wake_all, wake_one};
 
 use crate::mutex::MutexGuard;
 
-struct Condvar {
+pub struct Condvar {
     counter: AtomicU32,
+    waiter: AtomicUsize,
 }
 
 impl Condvar {
     pub const fn new() -> Self {
         Self {
             counter: AtomicU32::new(0),
+            waiter: AtomicUsize::new(0),
         }
     }
 
     pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
         let counter = self.counter.load(Ordering::Relaxed);
+        self.waiter.fetch_add(1, Ordering::Relaxed);
 
         let mutex = guard.lock;
         drop(guard);
 
         wait(&self.counter, counter);
+        self.waiter.fetch_sub(1, Ordering::Relaxed);
 
         mutex.lock()
     }
 
     pub fn notify_one(&self) {
+        if self.waiter.load(Ordering::Relaxed) == 0 {
+            return;
+        }
         self.counter.fetch_add(1, Ordering::Relaxed);
         wake_one(&self.counter);
     }
 
     pub fn notify_all(&self) {
+        if self.waiter.load(Ordering::Relaxed) == 0 {
+            return;
+        }
         self.counter.fetch_add(1, Ordering::Relaxed);
         wake_all(&self.counter);
     }
@@ -50,9 +60,9 @@ mod tests {
 
         std::thread::scope(|s| {
             s.spawn(|| {
-                for _ in 0..10 {
+                for _ in 0..100000 {
                     let mut q = queue.lock();
-                    let item = loop {
+                    let _item = loop {
                         if let Some(item) = q.pop_front() {
                             break item;
                         } else {
@@ -60,14 +70,13 @@ mod tests {
                         }
                     };
                     drop(q);
-                    dbg!(item);
                 }
             });
 
-            for i in 0..10 {
+            for i in 0..100000 {
                 queue.lock().push_back(i);
                 not_empty.notify_one();
-                std::thread::sleep(std::time::Duration::from_millis(100));
+                std::thread::sleep(std::time::Duration::from_millis(1));
             }
         });
     }
