7ba0f09b04cfd07632f19a1d859f85e4a22b6b77
俺不叫化咕龙 <1801943622@qq.com>
2025-07-27 18:53:30 +0800

    Implement read and write methods for RwLock with atomic wait and wake functionality

--- src/rw_lock/mod.rs
+++ src/rw_lock/mod.rs
@@ -1,8 +1,11 @@
 use std::{
     cell::UnsafeCell,
     ops::{Deref, DerefMut},
-    sync::atomic::AtomicU32,
+    sync::atomic::{AtomicU32, Ordering},
+    u32,
 };
+
+use atomic_wait::{wait, wake_all, wake_one};
 
 pub struct RwLock<T> {
     state: AtomicU32,
@@ -47,5 +50,103 @@ impl<T> RwLock<T> {
             state: AtomicU32::new(0),
             value: UnsafeCell::new(value),
         }
+    }
+
+    pub fn read(&self) -> ReadGuard<'_, T> {
+        let mut state = self.state.load(Ordering::Relaxed);
+        loop {
+            if state < u32::MAX {
+                assert!(state != u32::MAX - 1, "too many readers");
+                match self.state.compare_exchange_weak(
+                    state,
+                    state + 1,
+                    Ordering::Acquire,
+                    Ordering::Relaxed,
+                ) {
+                    Ok(_) => {
+                        return ReadGuard { lock: self };
+                    }
+                    Err(new_state) => {
+                        state = new_state;
+                    }
+                }
+            }
+            if state == u32::MAX {
+                wait(&self.state, u32::MAX);
+                state = self.state.load(Ordering::Relaxed);
+            }
+        }
+    }
+
+    pub fn write(&self) -> WriteGuard<'_, T> {
+        while let Err(e) =
+            self.state
+                .compare_exchange_weak(0, u32::MAX, Ordering::Acquire, Ordering::Relaxed)
+        {
+            wait(&self.state, e);
+        }
+        WriteGuard { lock: self }
+    }
+}
+
+impl<T> Drop for ReadGuard<'_, T> {
+    fn drop(&mut self) {
+        if self.lock.state.fetch_sub(1, Ordering::Relaxed) == 1 {
+            wake_one(&self.lock.state);
+        }
+    }
+}
+
+impl<T> Drop for WriteGuard<'_, T> {
+    fn drop(&mut self) {
+        self.lock.state.store(0, Ordering::Release);
+        wake_all(&self.lock.state);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::RwLock;
+
+    #[test]
+    fn test_single_thread() {
+        let rw = RwLock::new(vec![1, 2, 3]);
+
+        let r = rw.read();
+        assert_eq!(r.len(), 3);
+
+        let r2 = rw.read();
+        assert_eq!(r.len(), 3);
+
+        drop(r);
+        drop(r2);
+
+        let mut w = rw.write();
+        w.push(4);
+        drop(w);
+
+        let r = rw.read();
+        assert_eq!(r.len(), 4);
+    }
+
+    #[test]
+    fn test_multi_thread() {
+        let rw = RwLock::new(vec![]);
+
+        std::thread::scope(|s| {
+            s.spawn(|| {
+                let mut w = rw.write();
+                w.push(1);
+                w.push(2);
+            });
+
+            s.spawn(|| {
+                std::thread::sleep(std::time::Duration::from_millis(100));
+                let r1 = rw.read();
+                println!("{:?}", *r1);
+                let r2 = rw.read();
+                println!("{:?}", *r2);
+            });
+        })
     }
 }
