9ee2ee8ea0d36e05b2bfff4662068ba25960f3e2
俺不叫化咕龙 <1801943622@qq.com>
2025-09-30 21:34:24 +0800

    Refactor StrSplit iterator to use a Delimiter trait for improved flexibility and add tests for character and unicode delimiters

--- src/safe/iter.rs
+++ src/safe/iter.rs
@@ -1,18 +1,36 @@
-pub struct StrSplit<'haystack, 'delimiter> {
+pub struct StrSplit<'haystack, D> {
     remainder: Option<&'haystack str>,
-    delimiter: &'delimiter str,
+    delimiter: D,
 }
 
-impl<'haystack> Iterator for StrSplit<'haystack, '_> {
+trait Delimiter {
+    fn find_next(&self, haystack: &str) -> Option<(usize, usize)>;
+}
+
+impl Delimiter for &str {
+    fn find_next(&self, haystack: &str) -> Option<(usize, usize)> {
+        haystack.find(self).map(|index| (index, index + self.len()))
+    }
+}
+
+impl Delimiter for char {
+    fn find_next(&self, haystack: &str) -> Option<(usize, usize)> {
+        haystack
+            .find(*self)
+            .map(|index| (index, index + self.len_utf8()))
+    }
+}
+
+impl<'haystack, D: Delimiter> Iterator for StrSplit<'haystack, D> {
     type Item = &'haystack str;
 
     fn next(&mut self) -> Option<Self::Item> {
         if let Some(remainder) = self.remainder {
-            let Some(index) = remainder.find(self.delimiter) else {
+            let Some((start, end)) = self.delimiter.find_next(remainder) else {
                 return self.remainder.take();
             };
-            self.remainder = Some(&remainder[index + self.delimiter.len()..]);
-            Some(&remainder[..index])
+            self.remainder = Some(&remainder[end..]);
+            Some(&remainder[..start])
         } else {
             None
         }
@@ -61,5 +79,34 @@ mod tests {
         assert_eq!(splitter.next(), Some("d"));
         assert_eq!(splitter.next(), Some("e"));
         assert_eq!(splitter.next(), None);
+    }
+
+    #[test]
+    pub fn split_by_char() {
+        let s = "hello world,this is rust";
+        let mut iter = super::StrSplit {
+            remainder: Some(s),
+            delimiter: ' ',
+        };
+        assert_eq!(iter.next(), Some("hello"));
+        assert_eq!(iter.next(), Some("world,this"));
+        assert_eq!(iter.next(), Some("is"));
+        assert_eq!(iter.next(), Some("rust"));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    pub fn test_unicode_delimiter() {
+        let s = "I love rust🌍Rust is awesome🌍Let's code!❤";
+        let mut iter = super::StrSplit {
+            remainder: Some(s),
+            delimiter: '🌍',
+        };
+        assert!('🌍'.len_utf8() == 4);
+        assert!('❤'.len_utf8() == 3);
+        assert_eq!(iter.next(), Some("I love rust"));
+        assert_eq!(iter.next(), Some("Rust is awesome"));
+        assert_eq!(iter.next(), Some("Let's code!❤"));
+        assert_eq!(iter.next(), None);
     }
 }
