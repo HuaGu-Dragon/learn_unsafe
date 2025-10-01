202a09128db8cdb1e501e3c00afb2a00e5302163
俺不叫化咕龙 <1801943622@qq.com>
2025-10-01 18:04:21 +0800

    Implement next_back method for Flatten iterator and add tests for double-ended iteration

--- src/safe/flatten.rs
+++ src/safe/flatten.rs
@@ -53,7 +53,19 @@ where
     <O::Item as IntoIterator>::IntoIter: DoubleEndedIterator,
 {
     fn next_back(&mut self) -> Option<Self::Item> {
-        todo!()
+        loop {
+            if let Some(ref mut inner) = self.back_iter {
+                if let Some(item) = inner.next_back() {
+                    break Some(item);
+                }
+                self.back_iter = None;
+            }
+            if let Some(next_outer) = self.outer.next_back() {
+                self.back_iter = Some(next_outer.into_iter());
+            } else {
+                break self.front_iter.as_mut()?.next_back();
+            }
+        }
     }
 }
 
@@ -101,5 +113,45 @@ mod tests {
         assert_eq!(iter.next(), Some(5));
         assert_eq!(iter.next(), Some(6));
         assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    pub fn flatten_double_ended() {
+        let v = vec![vec![1, 2, 3], vec![4, 5, 6]];
+        let mut iter = super::Flatten::new(v.into_iter());
+        assert_eq!(iter.next_back(), Some(6));
+        assert_eq!(iter.next_back(), Some(5));
+        assert_eq!(iter.next_back(), Some(4));
+        assert_eq!(iter.next_back(), Some(3));
+        assert_eq!(iter.next_back(), Some(2));
+        assert_eq!(iter.next_back(), Some(1));
+        assert_eq!(iter.next_back(), None);
+    }
+
+    #[test]
+    pub fn flatten_double_ended_mixed() {
+        let v = vec![vec![1, 2, 3], vec![4, 5, 6]];
+        let mut iter = super::Flatten::new(v.into_iter());
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next_back(), Some(6));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next_back(), Some(5));
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next_back(), Some(4));
+        assert_eq!(iter.next_back(), None);
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    pub fn infinite_iter() {
+        let v = (1..).map(|i| 0..i);
+        let mut iter = super::Flatten::new(v);
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(0));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(0));
     }
 }
