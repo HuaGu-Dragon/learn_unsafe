6d5624fc86bd90016f5617fdda068c0d1942b01c
俺不叫化咕龙 <1801943622@qq.com>
2025-10-05 22:16:38 +0800

    Refactor Sender and Receiver to use shared Arc<Shared<T>> for improved message passing

--- src/safe/channel.rs
+++ src/safe/channel.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 pub struct Sender<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
 }
 
 unsafe impl<T: Send> Send for Sender<T> {}
@@ -13,15 +13,39 @@ unsafe impl<T: Send> Sync for Sender<T> {}
 
 impl<T> Sender<T> {
     pub fn send(&self, value: T) {
-        let mut queue = self.inner.queue.lock().unwrap();
-        queue.push_back(value);
-        drop(queue);
-        self.inner.available.notify_one();
+        let mut shared = self.shared.inner.lock().unwrap();
+        shared.queue.push_back(value);
+        drop(shared);
+        self.shared.available.notify_one();
+    }
+}
+
+impl<T> Clone for Sender<T> {
+    fn clone(&self) -> Self {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders += 1;
+        drop(inner);
+
+        Self {
+            shared: Arc::clone(&self.shared),
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.senders -= 1;
+        let no_senders = inner.senders == 0;
+        drop(inner);
+        if no_senders {
+            self.shared.available.notify_one();
+        }
     }
 }
 
 pub struct Receiver<T> {
-    inner: Arc<Inner<T>>,
+    shared: Arc<Shared<T>>,
     // impl !Send and !Sync
     marker: PhantomData<*const T>,
 }
@@ -30,36 +54,64 @@ pub struct Receiver<T> {
 unsafe impl<T: Send> Send for Receiver<T> {}
 
 impl<T> Receiver<T> {
-    pub fn recv(&self) -> T {
-        let mut queue = self.inner.queue.lock().unwrap();
+    pub fn recv(&self) -> Option<T> {
+        let mut shared = self.shared.inner.lock().unwrap();
         loop {
-            if let Some(value) = queue.pop_front() {
-                break value;
+            match shared.queue.pop_front() {
+                Some(value) => break Some(value),
+                None if shared.senders == 0 => break None,
+                None => shared = self.shared.available.wait(shared).unwrap(),
             }
-            queue = self.inner.available.wait(queue).unwrap();
         }
     }
 }
 
-struct Inner<T> {
-    queue: Mutex<VecDeque<T>>,
+struct Shared<T> {
+    inner: Mutex<Inner<T>>,
     available: Condvar,
 }
 
+struct Inner<T> {
+    queue: VecDeque<T>,
+    senders: usize,
+}
+
 pub fn channel<T>() -> (Sender<T>, Receiver<T>) {
-    let inner = Inner {
-        queue: Mutex::new(VecDeque::new()),
+    let inner = Shared {
+        inner: Mutex::new(Inner {
+            queue: VecDeque::new(),
+            senders: 1,
+        }),
         available: Condvar::new(),
     };
 
     let inner = Arc::new(inner);
     (
         Sender {
-            inner: inner.clone(),
+            shared: inner.clone(),
         },
         Receiver {
-            inner,
+            shared: inner,
             marker: PhantomData,
         },
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::safe::channel::channel;
+
+    #[test]
+    fn ping_pong() {
+        let (tx, rx) = channel();
+        tx.send(42);
+        assert_eq!(rx.recv(), Some(42));
+    }
+
+    #[test]
+    fn closed() {
+        let (tx, rx) = channel::<i32>();
+        drop(tx);
+        assert_eq!(rx.recv(), None);
+    }
 }
